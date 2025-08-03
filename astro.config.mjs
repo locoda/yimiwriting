530a7159fe93bb168f172ffe8412f7126ba1a240
Linda Huang <hhuang1206@gmail.com>
2025-08-02 20:06:33 -0700

    change color profile; never dark; compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import playformCompress from '@playform/compress';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [playformCompress()]
+});
