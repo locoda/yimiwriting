9c2bd4a3b056beb7c25b880f4d7dec84dd9874d6
Linda Huang <hhuang1206@gmail.com>
2025-08-03 11:03:54 -0700

    more changes adopt to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,22 +32,7 @@ module.exports = {
         body: ['"Libertinus Serif"', 'system-ui', 'serif'],
         brand: ['"Libertinus Sans"', 'system-ui', 'sans-serif'],
       },
-      fontSize: {
-        sm: '0.875rem',
-        base: '1rem',
-        md: '1.125rem',
-        lg: '1.25rem',
-        xl: '1.625rem',
-        '2xl': '2.125rem',
-        '3xl': '2.625rem',
-        '4xl': '3.5rem',
-        '5xl': '4.5rem',
-      },
-      boxShadow: {
-        sm: '0px 6px 3px rgba(9, 11, 17, 0.01), 0px 4px 2px rgba(9, 11, 17, 0.01), 0px 2px 2px rgba(9, 11, 17, 0.02), 0px 0px 1px rgba(9, 11, 17, 0.03)',
-        md: '0px 28px 11px rgba(9, 11, 17, 0.01), 0px 16px 10px rgba(9, 11, 17, 0.03), 0px 7px 7px rgba(9, 11, 17, 0.05), 0px 2px 4px rgba(9, 11, 17, 0.06)',
-        lg: '0px 62px 25px rgba(9, 11, 17, 0.01), 0px 35px 21px rgba(9, 11, 17, 0.05), 0px 16px 16px rgba(9, 11, 17, 0.1), 0px 4px 9px rgba(9, 11, 17, 0.12)',
-      },
+      
     },
   },
   plugins: [],
