28e3f19290cdad589d71bea235574075ad12b768
Linda Huang <hhuang1206@gmail.com>
2025-08-16 13:53:15 -0700

    basic color chagnes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,18 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+        colors: {
+        hh15: {
+          bg: "#faeee7",
+          fg: "#33272a",
+          muted: "#594a4e",
+          card: "#fffffe",
+          accent: "#ff8ba7",
+          accent2: "#ffc6c7",
+          accent3: "#c3f0ca",
+          border: "#f1e5de",
+        },
+      },
       fontFamily: {
         sans: [
           "Bricolage Grotesque Variable",
