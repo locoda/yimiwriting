3dbef24495aca76274096e8b563dadcd26624c0e
Linda Huang <hhuang1206@gmail.com>
2025-08-16 14:42:02 -0700

    add some bg and adjust a few small changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   site: "https://astroship.web3templates.com",
-  integrations: [tailwind(), mdx(), sitemap(), icon()],
+  integrations: [tailwind(), mdx(), sitemap(), icon(), (await import("@playform/compress")).default()],
 });
