6a003211750047d1e4d3b146775b71cbff1ad7d2
Linda Huang <hhuang1206@gmail.com>
2025-08-16 13:29:00 -0700

    change of work links

--- src/content/config.ts
+++ src/content/config.ts
@@ -12,9 +12,8 @@ const workCollection = defineCollection({
       alt: z.string(),
     }),
     publishDate: z.string().transform(str => new Date(str)),
-    author: z.string().default('Astroship'),
     category: z.string(),
-    tags: z.array(z.string()),
+    external: z.string().optional()
   }),
 });
 
