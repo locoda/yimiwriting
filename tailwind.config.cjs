6c4f4c0dc7949e2f94676221a1ad222aed700f87
Linda Huang <hhuang1206@gmail.com>
2025-09-07 16:55:56 -0700

    feat: use Gravitas One for brand font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -19,7 +19,7 @@ module.exports = {
       fontFamily: {
         serif: ['"Libertinus Serif"', 'Times', ...defaultTheme.fontFamily.serif],
         sans: ['"Libertinus Sans"', 'Noto Sans SC', 'system-ui', ...defaultTheme.fontFamily.sans],
-        brand: ['"Permanent Marker"', ...defaultTheme.fontFamily.serif],
+        brand: ['"Gravitas One"', ...defaultTheme.fontFamily.serif],
       }
     },
   },
