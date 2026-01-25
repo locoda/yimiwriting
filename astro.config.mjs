d78c8b4c864a4989eb5de0392709b8e3e9116376
Linda Huang <hhuang1206@gmail.com>
2026-01-24 16:55:49 -0800

    replace astro-icon with font awesome to avoid security issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,9 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astroship.web3templates.com",
-  integrations: [tailwind(), mdx(), sitemap(), icon(), (await import("@playform/compress")).default()],
+  integrations: [tailwind(), mdx(), sitemap(),  (await import("@playform/compress")).default()],
 });
