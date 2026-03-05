b62eaa43697bdb308d3a3f7afd1be940f9c02156
Linda Huang <hhuang1206@gmail.com>
2026-03-04 21:12:38 -0800

    switch Astro Tailwind v4 integration to Vite plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astroship.web3templates.com",
-  integrations: [tailwind(), mdx(), sitemap(), (await import("@playform/compress")).default()],
+  integrations: [mdx(), sitemap(), (await import("@playform/compress")).default()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
