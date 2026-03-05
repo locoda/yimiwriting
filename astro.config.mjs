74908f2f71a7f622e461ee7ba09a3b9c672c043d
Linda Huang <hhuang1206@gmail.com>
2026-03-04 23:55:56 -0800

    upgrade tailwind css to 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astroship.web3templates.com",
-  integrations: [tailwind(), mdx(), sitemap(),  (await import("@playform/compress")).default()],
+  integrations: [mdx(), sitemap(),  (await import("@playform/compress")).default()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
